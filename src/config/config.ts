ae82c94b6ff06b64899eca52d897971447d504a6
devsenpai09@gmail.com <devsenpai09@gmail.com>
2024-03-21 09:22:06 +0100

    create env

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,9 +1,8 @@
 export default function config() {
-  const { API_BASE_URL, TOKEN_EXPIRE_AT_MS, NODE_ENV } = process.env;
-
   return {
-    apiBaseUrl: API_BASE_URL || "https://api.development.goziri.com/v1",
-    tokenExpireAtMs: TOKEN_EXPIRE_AT_MS,
-    environment: NODE_ENV,
+    apiBaseUrl:
+      process.env.NEXT_PUBLIC_API_BASE_URL ||
+      "https://api.development.goziri.com/v1",
+    tokenExpireAtMs: process.env.NEXT_PUBLIC_TOKEN_EXPIRE_AT_MS,
   };
 }
